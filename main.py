9943ddabaa4ac8fa4c5d8e86c2f8c459f8d96faf
Yueyang Pan <yueyangpan@gmail.com>
2020-02-02 20:10:36 -0500

    adding project

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     """Return a friendly HTTP greeting."""
-    return 'Hello World!'
+    return 'Hello everyone, I am Yueyang Pan. '
 
 @app.route('/name/<value>')
 def name(value):
